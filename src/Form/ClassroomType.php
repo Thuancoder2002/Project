2ac9d21ef5cc55e6cec8ec8dc54ed37db7f625ad
Thuancoder2002 <97907249+DangDucThuan38@users.noreply.github.com>
2022-08-13 22:35:43 +0700

    ok

--- src/Form/ClassroomType.php
+++ src/Form/ClassroomType.php
@@ -2,19 +2,54 @@
 
 namespace App\Form;
 
+use App\Entity\Major;
+use App\Entity\Course;
 use App\Entity\Classroom;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ClassroomType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('describle')
-            ->add('major')
+            ->add('name', TextType::class,
+                [
+                    'label' => 'Name Class',
+                    'required' => true,
+                    'attr' => [
+                        'minlength' => 5,
+                        'maxlength' => 50,
+                    ]
+                ])
+            ->add('describle',TextType::class,
+                [
+                    'label' => 'Describle',
+                    'required' => true,
+                    'attr' => [
+                        'minlength' => 5,
+                        'maxlength' => 50,
+                    ]
+                ])
+            ->add('major', EntityType::class, [
+                'label' => 'Major',
+                'required' => true,
+                'class' => Major::class,
+                'choice_label' => 'name',
+                'multiple' => false,
+                'expanded' => true
+            ])
+             ->add(
+                'save',
+                SubmitType::class,
+                [
+                    'label' => 'Save'
+                ]
+            )
         ;
     }
 
