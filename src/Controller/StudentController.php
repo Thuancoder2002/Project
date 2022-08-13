03bc6ddcda2fd31e636e853cdce72915f0919016
Thuancoder2002 <97907249+DangDucThuan38@users.noreply.github.com>
2022-08-13 21:42:03 +0700

    ok

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -2,17 +2,150 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Course;
+use App\Entity\Student;
+use App\Form\StudentType;
+use App\Repository\StudentRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
+#[Route('/student')]
 class StudentController extends AbstractController
 {
-    #[Route('/student', name: 'app_student')]
-    public function index(): Response
+    
+       #[Route('/', name: 'student_index')]
+    public function student_index()
     {
+        $student=$this->getDoctrine()->getRepository(Student::class)->findAll();
+        $course = $this->getDoctrine()->getRepository(Course::class)->findAll();
+
+        if (!$student) {
+            throw $this->createNotFoundException(
+                'No students found in the database.'
+            );
+        }
         return $this->render('student/index.html.twig', [
-            'controller_name' => 'StudentController',
+            'student' => $student,
+            'course'=>$course
         ]);
     }
+
+
+          #[Route('/detail/{id}', name: 'student_detail')]
+       public function studentDetail($id){
+      $student = $this->getDoctrine()->getRepository(Student::class)->find($id);
+         if($student!=null){
+               return $this->render('student/detail.html.twig',[
+                'student'=>$student
+            ]);  
+            
+         }else{
+            $this->addFlash(
+               'Error',
+               'Todo not found.Please try again!'
+            );
+            return $this->redirectToRoute('student_index');
+        }
+
+       }
+
+
+
+    #[Route('/add', name: 'student_add')]
+    public function sdtudentadd(Request $request){
+            $student= new Student; 
+            $form=$this->createForm(StudentType::class,$student);
+            $form->handleRequest($request);
+            if($form->isSubmitted() && $form->isValid()){
+                $manager=$this->getDoctrine()->getManager();
+                $manager->persist($student);
+             
+                $manager->flush();
+                $this->addFlash(
+               'Success',
+               'You have Successfully added'
+            );  
+            return $this->redirectToRoute("student_index");
+
+            }
+            return $this->renderForm("student/add.html.twig",[
+                'studentForm'=>$form
+            ]);
+
+
+
+    }
+ 
+     #[Route('/delete/{id}', name: 'student_delete')]
+     public function studentDelete ($id, ManagerRegistry $managerRegistry) {
+     $student = $managerRegistry->getRepository(Student::class)->find($id);
+     if ($student == null) {
+        $this->addFlash('Warning', 'Book not existed !');
+     } else {
+        $manager = $managerRegistry->getManager();
+        $manager->remove($student);
+        $manager->flush();
+        $this->addFlash('Info', 'Delete book succeed !');
+     }
+     return $this->redirectToRoute('student_index');
+   }
+
+       
+       #[Route('/edit/{id}', name: 'student_edit')]
+       public function studentEdit ($id, Request $request) {
+        $student = $this->getDoctrine()->getRepository(Student::class)->find($id);
+        if ($student== null) {
+            $this->addFlash('Warning', 'Book not existed !');
+         } else {
+            $form = $this->createForm(StudentType::class,$student);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($student);
+                $manager->flush();
+                $this->addFlash('Info', 'Edit book succeed !');
+                return $this->redirectToRoute("student_index");
+            }
+            return $this->renderForm("student/edit.html.twig",
+            [
+                'studentForm' => $form
+            ]);
+         }
+     }
+
+     #[Route('/asc', name: 'student_asc')]
+   public function sortAsc(StudentRepository $studentRepository, ManagerRegistry $registry) {
+       $student = $studentRepository->sortStudentAsc();
+       return $this->render("student/index.html.twig",
+                            [
+                                'student' => $student,
+                            ]);
+   }
+
+   #[Route('/desc', name: 'student_desc')]
+   public function sortDesc(StudentRepository $studentRepository, ManagerRegistry $registry) {
+      
+       $student = $studentRepository->sortStudentDesc();
+       return $this->render("student/index.html.twig",
+                            [
+                                'student' => $student
+                            ]);
+   }
+
+   #[Route('/search', name: 'student_search')]
+   public function search (Request $request, StudentRepository $studentRepository, ManagerRegistry $registry) {
+       $keyword = $request->get('name');
+       $student = $studentRepository->search($keyword);
+       return $this->render("student/index.html.twig",
+                            [
+                                'student' => $student,
+                            ]);
+   }
+
+
+
 }
--- src/Form/StudentType.php
+++ src/Form/StudentType.php
@@ -2,25 +2,118 @@
 
 namespace App\Form;
 
+use App\Entity\Grade;
+use App\Entity\Major;
+use App\Entity\Course;
 use App\Entity\Student;
+use App\Entity\Classroom;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class StudentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('image')
-            ->add('brithday')
-            ->add('email')
-            ->add('phone')
-            ->add('studentid')
-            ->add('courses')
-            ->add('major')
-            ->add('classroom')
+                  ->add(
+                'name',
+                TextType::class,
+                [
+                    'label' => 'Student Name',
+                    'required' => true,
+                    'attr' => [
+                        'minlength' => 5,
+                        'maxlength' => 50,
+                    ]
+                ]
+            )
+            ->add(
+                'email',
+                TextType::class,
+                [
+                    'label' => 'Student Email',
+                    'required' => true
+                ]
+            )
+            ->add(
+                'studentId',
+                TextType::class,
+                [
+                    'label' => 'Student ID',
+                    'required' => true
+                ]
+            )
+            ->add(
+                'brithday',
+                DateType::class,
+                [
+                    'label' => 'Student Birthday',
+                    'required' => true,
+                    'widget' => 'single_text'
+                ]
+            )
+            ->add(
+                'phone',
+                TextType::class,
+                [
+                    'label' => 'Student Phone',
+                    'required' => true
+                ]
+            )
+
+            ->add(
+                'image',
+                TextType::class,
+                [
+                    'label' => 'Student Image',
+                    'required' => true
+                ]
+            )
+            ->add('courses', EntityType::class, [
+                'label' => 'Course Name',
+                'required' => true,
+                'class' => Course::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true
+            ])
+              ->add('major', EntityType::class, [
+                'label' => 'Major Name',
+                'required' => true,
+                'class' => Major::class,
+                'choice_label' => 'name',
+                'multiple' => false,
+                'expanded' => false
+            ])
+             ->add('classroom', EntityType::class, [
+                'label' => 'Class',
+                'required' => true,
+                'class' => Classroom::class,
+                'choice_label' => 'name',
+                'multiple' => false,
+                'expanded' => false
+            ])
+             ->add('grades', EntityType::class, [
+                'label' => 'Grades',
+                'required' => true,
+                'class' => Grade::class,
+                'choice_label' => 'grade',
+                'multiple' => true,
+                'expanded' => false
+            ])            
+            ->add(
+                'save',
+                SubmitType::class,
+                [
+                    'label' => 'Save'
+                ]
+            )
+            
         ;
     }
 
