721d24790c80f7946e566ea6599c634903ba9953
ThaiTuanBTT <TuanBTTGCH200717@fpt.edu.vn>
2022-08-15 01:55:05 +0700

    tuan

--- src/Form/MajorType.php
+++ src/Form/MajorType.php
@@ -3,17 +3,57 @@
 namespace App\Form;
 
 use App\Entity\Major;
+use App\Entity\Student;
+use App\Entity\Classroom;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class MajorType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('describle')
+            ->add('name', TextType::class,[
+                  'label' => 'Name Major',
+                    'required' => true,
+                    'attr' => [
+                        'minlength' => 5,
+                        'maxlength' => 50,
+                    ]
+            ])
+            ->add('describle', TextType::class,[
+                  'label' => 'Describle of Major',
+                    'required' => true,
+                    'attr' => [
+                        'minlength' => 5,
+                        'maxlength' => 50,
+                    ]
+
+
+            ])
+
+        ->add('student', EntityType::class, [
+                'label' => 'Course Name',
+                'required' => true,
+                'class' => Student::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true
+            ])
+
+
+           
+              ->add(
+                'save',
+                SubmitType::class,
+                [
+                    'label' => 'Add Major'
+                ]
+            )
         ;
     }
 
